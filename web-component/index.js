bb2a80db4fb71283b738332a8d69a7002023640c
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-05-07 16:11:55 +0900

    Fix: Card Component 생성 및 렌더링 로직 수정

--- web-component/index.js
+++ web-component/index.js
@@ -1,12 +1,23 @@
 import Card from "./card.js";
 
-const loaded = async () => {
-  const cards = await fetch("./card.json").then((res) => res.json());
+// Card 데이터 패칭
+const getCardsJson = async () => {
+  return fetch("./card.json").then((res) => res.json());
+};
 
+// Card 리스트 Wrapper 생성
+const createCardsWrapper = () => {
   const wrapper = document.createElement("div");
   wrapper.classList.add("card-wrapper");
+  return wrapper;
+};
 
-  cards.map((card) => {
+// DOMContent 로드 후, 렌더링
+const loaded = async () => {
+  const cards = await getCardsJson();
+  const wrapper = createCardsWrapper();
+
+  cards.forEach((card) => {
     const cardComponent = new Card();
     const number = String(card.id).padStart(2, "0");
     cardComponent.setContent(`${card.content} ${number}`);
